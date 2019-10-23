0164d76b3dc400d350c83977c82a938118e057e4
Marc Maccaferri <marc.m.maccaferri@gmail.com>
2019-10-23 11:39:41 -0400

    updated the view with partials

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -4,11 +4,7 @@
 <head>
     {{-- CALL IN THE MAIN HEAD --}}
     @include("layouts.partials.head.mainHead")
-    <link href="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.js"></script>
-    <script src="http://netdna.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.js"></script>
-    <link href="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.css" rel="stylesheet">
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/summernote/0.8.12/summernote.js"></script>
+    @include("layouts.partials.head.summernote")
     <link href="{{ asset('css/post.css') }}" rel="stylesheet">
 
 </head>
